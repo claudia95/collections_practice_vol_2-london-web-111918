f693d8229b96ca0eb92409860a4a071c89f9eb4b
Claudia Fox <claudiafox95@icloud.com>
2018-11-15 13:28:34 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
 # your code goes here
+def begins_with_r(array)
+ array.all?{|word| word.chr == "r"} 
+ end
+
+def contain_a(array)
+  array.select { |word| word.include?("a") }
+end
+
+def first_wa(array)
+  array.find{|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.keep_if{|item| item.is_a?(String)}
+end
+
+def count_elements(array)
+  new_array = []
+  array.each {|element| new_array << array.count(element)}
+  counter = 0 
+  array.each do |element|
+    element[:count] = new_array[counter]
+    counter += 1
+  end
+  array.uniq 
+end
+  
+
+def merge_data(keys, data)
+  array_1 = []
+  array_2 = []
+end
+
+def find_cool
+  
+end
+
+def organize_schools
+  
+end
